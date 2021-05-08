d123d1f47f0067ce3dc95f9b0d6ec31d3be949ef
M. Javad Mohebbi <mohammadjavadmohebbi@gmail.com>
2021-05-08 17:03:10 +0430

    Sign In

--- server/auth_routes.go
+++ server/auth_routes.go
@@ -4,6 +4,6 @@ import "github.com/gorilla/mux"
 
 func (s *Server) AuthRoutes(r *mux.Router) {
 
-	// login
+	// sign in to system and get jwt token from server
 	r.HandleFunc("/login", s.LoginController).Methods("POST", "OPTIONS")
 }
